9aa1338511660d19e52bb968c1815ab661921038
gordeyst <gordeystahovets@gmail.com>
2020-03-29 20:06:45 +0300

    init the massive of srtructures

---
+++ script.js
@@ -0,0 +1,6 @@
+const userData = [
+  { id: 1, name: "Gordey", job: "Frontend", age: 18 },
+  { id: 2, name: "Vladislav", job: "Fullstack", age: 27 },
+  { id: 3, name: "Anton", job: "Backend", age: 22 },
+  { id: 4, name: "Helen", job: "Student", age: 20 }
+];
